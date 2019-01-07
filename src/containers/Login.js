7bd91c1708731532a829bbed0a72cef8089b2218
ellymaimon <eliranmaimon@gmail.com>
2019-01-06 22:34:33 -0800

    Replace button with new loader button, add isLoading flag, redirect on submit

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Auth } from 'aws-amplify';
-import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import LoaderButton from '../components/LoaderButton';
 import './Login.css';
 
 export default class Login extends Component {
@@ -8,6 +9,7 @@ export default class Login extends Component {
     super(props);
 
     this.state = {
+      isLoading: false,
       email: '',
       password: ''
     };
@@ -25,14 +27,16 @@ export default class Login extends Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
+    this.setState({ isLoading: true });
     try {
       await Auth.signIn(this.state.email, this.state.password);
       this.props.userHasAuthenticated(true);
+      this.props.history.push('/');
     } catch (e) {
       alert(e.message);
+      this.setState({ isLoading: false });
     }
-  }
+  };
 
   render() {
     return (
@@ -55,14 +59,15 @@ export default class Login extends Component {
               type='password'
             />
           </FormGroup>
-          <Button
+          <LoaderButton
             block
             bsSize='large'
             disabled={!this.validateForm()}
             type='submit'
-          >
-            Login
-          </Button>
+            isLoading={this.state.isLoading}
+            text='Login'
+            loadingText='Logging in...'
+          />
         </form>
       </div>
     );
